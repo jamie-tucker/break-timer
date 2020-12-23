6d9f26885f5feba31b036087f29d0ddd4008f288
Jamie Tucker <jamie@uken.com>
2020-12-23 10:50:37 -0500

    Clean up some logic, get the highlighting working

--- BreakTimer/AppDelegate.swift
+++ BreakTimer/AppDelegate.swift
@@ -12,22 +12,28 @@ import SwiftUI
 class AppDelegate: NSObject, NSApplicationDelegate {
 
 //  var window: NSWindow!
-  var popover = NSPopover.init()
+  var popover: NSPopover!
   var statusBar: StatusBarController?
 
   func applicationDidFinishLaunching(_ aNotification: Notification) {
     // Create the SwiftUI view that provides the window contents.
     let contentView = ContentView()
 
-    self.popover.contentViewController = MainViewController()
-    self.popover.contentSize = NSSize(width: 360.0, height: 360.0)
-    self.popover.contentViewController = NSHostingController(rootView: contentView)
+    let popover = NSPopover()
+    popover.contentSize = NSSize(width: 360.0, height: 360.0)
+    popover.behavior = .transient
+    popover.contentViewController = NSHostingController(rootView: contentView)
+    self.popover = popover
 
     self.statusBar = StatusBarController.init(self.popover)
   }
 
   func applicationWillTerminate(_ aNotification: Notification) {
     // Insert code here to tear down your application
+  }
+
+  @objc func quit(sender: AnyObject) {
+    NSApp.terminate(self)
   }
 
 }
--- BreakTimer/Controllers/StatusBarController.swift
+++ BreakTimer/Controllers/StatusBarController.swift
@@ -7,65 +7,62 @@
 
 import SwiftUI
 
-class StatusBarController: NSObject, NSMenuDelegate {
-  private var statusBar: NSStatusBar
+class StatusBarController {
   private var statusItem: NSStatusItem
   private var popover: NSPopover
-  private var statusBarMenu: NSMenu!
+  private var menu: NSMenu!
   private var eventMonitor: EventMonitor?
 
   init(_ popover: NSPopover) {
     self.popover = popover
-    self.statusBar = NSStatusBar.init()
-    self.statusItem = self.statusBar.statusItem(withLength: 24.0)
-    super.init()
+    self.statusItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
 
     self.eventMonitor = EventMonitor(mask: [.leftMouseDown, .rightMouseDown], handler: self.mouseEventHandler)
 
     if let statusBarButton = self.statusItem.button {
-      statusBarButton.image = NSImage(named: NSImage.Name("StatusBarIcon"))
-      statusBarButton.image?.size = NSSize(width: 18.0, height: 18.0)
+      statusBarButton.image = NSImage(named: "StatusBarIcon")
       statusBarButton.image?.isTemplate = true
       statusBarButton.action = #selector(self.onButtonPressed(sender:))
-      statusBarButton.sendAction(on: [.rightMouseDown, .rightMouseUp, .leftMouseDown, .leftMouseUp])
+      statusBarButton.sendAction(on: [.rightMouseUp, .leftMouseUp])
       statusBarButton.target = self
     }
 
-    statusBarMenu = NSMenu()
-    statusBarMenu.delegate = self
-    statusBarMenu.addItem(
+    self.popover.animates = false
+
+    let menu = NSMenu()
+    menu.addItem(
       withTitle: "Preferences",
       action: nil,
-      keyEquivalent: ",").target = self
-    statusBarMenu.addItem(
+      keyEquivalent: ",")
+    menu.addItem(
       withTitle: "Quit",
-      action: #selector(exitNow(sender:)),
-      keyEquivalent: "q").target = self
+      action: #selector(AppDelegate.quit(sender:)),
+      keyEquivalent: "q")
+
+    self.menu = menu
   }
 
   @objc func onButtonPressed(sender: NSStatusBarButton) {
     let event = NSApp.currentEvent!
-    if event.type == NSEvent.EventType.rightMouseUp {
-      statusItem.menu = statusBarMenu
-      statusItem.button?.performClick(self.hidePopover(sender))
-    } else if event.type == NSEvent.EventType.rightMouseDown || event.type == NSEvent.EventType.leftMouseDown {
-      sender.highlight(true)
-    } else {
-      if popover.isShown {
-        self.hidePopover(sender)
-      } else {
-        self.showPopover(sender)
-      }
-    }
-  }
 
-  @objc func menuDidClose(_ menu: NSMenu) {
-    statusItem.menu = nil
+    switch (event.type, event.modifierFlags.contains(.control)) {
+    case (NSEvent.EventType.rightMouseUp, _),
+         (NSEvent.EventType.leftMouseUp, true):
+      statusItem.menu = menu
+      statusItem.button?.performClick(self.hidePopover(sender))
+      statusItem.menu = nil
+    default:
+        if popover.isShown {
+          self.hidePopover(sender)
+        } else {
+          self.showPopover(sender)
+        }
+    }
   }
 
   func showPopover(_ sender: AnyObject) {
     if let statusBarButton = self.statusItem.button {
-      self.popover.show(relativeTo: statusBarButton.bounds, of: statusBarButton, preferredEdge: NSRectEdge.maxY)
+      self.popover.show(relativeTo: statusBarButton.bounds, of: statusBarButton, preferredEdge: NSRectEdge.minY)
       eventMonitor?.start()
     }
   }
@@ -73,10 +70,6 @@ class StatusBarController: NSObject, NSMenuDelegate {
   func hidePopover(_ sender: AnyObject) {
     self.popover.performClose(sender)
     eventMonitor?.stop()
-  }
-
-  @objc func exitNow(sender: AnyObject) {
-    NSApplication.shared.terminate(self)
   }
 
   func mouseEventHandler(_ event: NSEvent?) {
