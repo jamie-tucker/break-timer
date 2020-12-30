7205b70462ec5c3b9337945f07f496cbbd31fe3e
Jamie Tucker <jamie@jamietucker.com>
2020-12-30 16:15:05 -0500

    add new view

---
+++ BreakTimer/Views/AlarmView.swift
@@ -0,0 +1,20 @@
+//
+//  AlarmView.swift
+//  BreakTimer
+//
+//  Created by Jamie Tucker on 2020-12-30.
+//
+
+import SwiftUI
+
+struct AlarmView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AlarmView_Previews: PreviewProvider {
+    static var previews: some View {
+        AlarmView()
+    }
+}
