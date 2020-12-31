5a0d309ef67f802520d07533cf654832c14241c9
Jamie Tucker <jamie@jamietucker.com>
2020-12-31 00:02:01 -0500

    add launcherApp

---
+++ LauncherApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  LauncherApp
+//
+//  Created by Jamie Tucker on 2020-12-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
